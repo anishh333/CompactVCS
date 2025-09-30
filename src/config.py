abc9a025f35bcd46022692c4381bfd4650b3105c
anish333 <anishreddydakareddygari@gmail.com>
2025-09-30 13:17:40 +0530

    streamlit changes

--- src/config.py
+++ src/config.py
@@ -1,16 +1,6 @@
-import os
-from dotenv import load_dotenv # pyright: ignore[reportMissingImports] 
-from supabase import create_client, Client # pyright: ignore[reportMissingImports] 
+import streamlit as st
+from supabase import create_client
 
-load_dotenv()  # loads .env from project root
- 
-SUPABASE_URL = os.getenv("supabase_url")
-SUPABASE_KEY = os.getenv("supabase_key")
- 
-def get_supabase() -> Client:
-    """
-    Return a supabase client. Raises RuntimeError if config missing.
-    """
-    if not SUPABASE_URL or not SUPABASE_KEY:
-        raise RuntimeError("SUPABASE_URL and SUPABASE_KEY must be set in environment (.env)")
-    return create_client(SUPABASE_URL, SUPABASE_KEY)
+url = st.secrets["supabase"]["supabase_url"]
+key = st.secrets["supabase"]["supabse_key"]
+supabase = create_client(url, key)
