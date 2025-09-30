fce9ef6f12c8f42a016235b855995653692aefaa
anish333 <anishreddydakareddygari@gmail.com>
2025-09-30 13:39:48 +0530

    API issues resolved

--- src/config.py
+++ src/config.py
@@ -1,6 +1,9 @@
 import streamlit as st
 from supabase import create_client
 
-url = st.secrets["supabase"]["supabase_url"]
-key = st.secrets["supabase"]["supabse_key"]
-supabase = create_client(url, key)
+def get_supabase():
+    url = st.secrets["supabase"]["supabse_url"]
+    key = st.secrets["supabase"]["supabase_key"]
+    return create_client(url, key)
+
+supabase = get_supabase()
